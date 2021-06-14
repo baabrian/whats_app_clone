298f15b2bb8055f5a126aacd3aece66c02d14222
Brian Nguyen <brianlnguyen12@gmail.com>
2021-06-13 17:38:21 -0700

    included openconversations comp

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { SideBar } from '../SideBar';
+import { OpenConversations } from '../OpenConversations';
+import { useConversations } from '../../contexts/ConversationsProvider';
 import './styles.css';
 
 export const Dashboard = ({ id }) => {
+  const { selectedConversation } = useConversations();
   return (
     <div className='d-flex dashboard__container'>
       <SideBar id={id} />
+      {selectedConversation && <OpenConversations />}
     </div>
   );
 };
